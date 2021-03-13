fb7e210322745f03fe4cfe3db37be46f8be58fa3
Maaack <marek.belski@gmail.com>
2021-03-13 12:34:54 -0800

    INIT-04: Characters adjust to the average of recent and all transactions.

--- Scenes/Characters/BaseCharacter.gd
+++ Scenes/Characters/BaseCharacter.gd
@@ -20,6 +20,7 @@ var price_point : float
 var current_price_point : float
 var home_position : Vector2
 var recent_transactions : Array = []
+var all_transactions : Array = []
 
 func set_home(position : Vector2):
 	home_position = position
@@ -42,6 +43,7 @@ func set_role(value : int):
 func set_price_point(value : float):
 	price_point = value
 	basic_bar_ui.set_base(price_point)
+	reset_history()
 	emit_signal("price_point_updated", price_point)
 
 func set_current_price_point(value : float):
@@ -61,20 +63,35 @@ func go_home(time_to : float = 1.0):
 func add_transaction(avg : float = 0.0):
 	recent_transactions.append(avg)
 
-func get_avg_of_transactions():
+func get_avg_of_array(values : Array):
 	var sum : float = 0.0
-	if recent_transactions.size() == 0:
+	if values.size() == 0:
 		return sum
-	for i in recent_transactions:
+	for i in values:
 		sum += i
-	return sum / recent_transactions.size()
+	return sum / values.size()
+
+func get_avg_of_all_transactions():
+	return get_avg_of_array(all_transactions)
+
+func get_avg_of_recent_transactions():
+	return get_avg_of_array(recent_transactions)
 
 func get_lower_expectations():
 	return (price_point + current_price_point) / 2
 
 func adjust_current_price_point():
-	var avg = get_avg_of_transactions()
+	var avg : float = get_avg_of_recent_transactions()
 	if avg == 0.0:
 		avg = get_lower_expectations()
+	var lifetime_avg : float = get_avg_of_all_transactions()
+	if lifetime_avg != 0.0:
+		avg = (avg + lifetime_avg) / 2.0
 	set_current_price_point(avg)
+	all_transactions += recent_transactions
 	recent_transactions.clear()
+
+func reset_history():
+	all_transactions.clear()
+	recent_transactions.clear()
+	current_price_point = price_point
